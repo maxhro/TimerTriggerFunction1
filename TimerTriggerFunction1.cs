8900ee607e8871377bdc4b14c02ed690d2cf4b5e
Maximilian Risch <mr@cloudeteer.de>
2024-10-09 09:20:10 +0200

    initial

---
+++ TimerTriggerFunction1.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MaxHRO.Function1
+{
+    public class TimerTriggerFunction1
+    {
+        private readonly ILogger _logger;
+
+        public TimerTriggerFunction1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TimerTriggerFunction1>();
+        }
+
+        [Function("TimerTriggerFunction1")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
